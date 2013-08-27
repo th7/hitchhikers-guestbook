d2184f442c87f91cb96f8d16bec8ea2cc3e0d4d1
TH <tylerhartland7@gmail.com>
2013-08-27 17:15:55 -0500

    user validates uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_many :entries
 
   validates_presence_of :username, :password_digest
+  validates_uniqueness_of :username
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe User do
   it { should have_many(:entries) }
+
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
+  it { should validate_uniqueness_of(:username) }
 
 end
