ba5cc3a1b406435b06839dd23bd60fce026ce286
TH <tylerhartland7@gmail.com>
2013-08-27 17:13:54 -0500

    user validates presence of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 
   has_many :entries
 
+  validates_presence_of :username
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe User do
   it { should have_many(:entries) }
+  it { should validate_presence_of(:username) }
 end
