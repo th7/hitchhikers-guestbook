cce255c8048177d3ec2f9794fad40109a94da0a7
TH <tylerhartland7@gmail.com>
2013-08-27 16:35:54 -0500

    rails new hitchhikers-guestbook -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HitchhikersGuestbook::Application.config.secret_key_base = '534b6cb41a5fa80e35fa2e0d7e3017ee0d322445e88dfc4a9ac256d82ae78564d107bf3c0770377d3c7ad691e42312e5b829a9c03a5cc07201828c50a8022cc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HitchhikersGuestbook::Application.config.session_store :cookie_store, key: '_hitchhikers-guestbook_session'
