f63b9c64b3953af26cf5d6a181d24ae73c359694
TH <tylerhartland7@gmail.com>
2013-08-27 17:21:12 -0500

    entry validates presence of location

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,5 @@ class Entry < ActiveRecord::Base
 
   belongs_to :user
 
-  validates_presence_of :message
+  validates_presence_of :message, :location
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -4,4 +4,5 @@ describe Entry do
   it { should belong_to(:user) }
 
   it { should validate_presence_of(:message) }
+  it { should validate_presence_of(:location) }
 end
