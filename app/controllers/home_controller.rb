c632183f09ccb8903214cf141419d8df2d3ca834
TH <tylerhartland7@gmail.com>
2013-08-28 10:09:39 -0500

    home#index assigns @entries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   skip_filter :auth, :only => [:index]
 
   def index
+    @entries = Entry.all
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,5 +5,10 @@ describe HomeController do
     it 'can be reached' do
       expect { get :index }.not_to raise_error
     end
+
+    it 'assigns @entries' do
+      get :index
+      expect(assigns(:entries)).not_to be_nil
+    end
   end
 end
