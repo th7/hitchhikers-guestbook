556a8c756493b8434fcdb266e9f78f4d1cf11d65
TH <tylerhartland7@gmail.com>
2013-08-28 11:01:27 -0500

    Entries#create creates an entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,13 @@
 class EntriesController < ApplicationController
   def create
+    entry = current_user.entries.new(entry_params)
+    entry.save
+    redirect_to root_path
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:message, :location)
   end
 end
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,14 +1,25 @@
 require 'spec_helper'
 
 describe EntriesController do
-  context 'not signed in' do
+  context 'signed in' do
     before do
-      session.clear
+      @test_user = FactoryGirl.create(:user)
+      session[:user_id] = @test_user.id
     end
 
     describe '#create' do
-      it 'can be reached' do
-        expect { post :create }.not_to raise_error
+      it 'creates a new entry' do
+        post :create, :entry => {
+          :message => 'testmessage',
+          :location => 'testlocation'
+        }
+
+        expect(Entry.where(
+          :message => 'testmessage',
+          :location => 'testlocation',
+          :user => @test_user
+          ).first
+        ).to be_kind_of Entry
       end
     end
   end
